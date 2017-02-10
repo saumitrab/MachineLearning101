1181628873d456d86b5e170044dc51a56c59c308
Saumitra Bhanage <sbhanage@groupon.com>
2017-02-09 23:00:41 -0800

    Compare decision tree and K neighbors

--- compare-algos.py
+++ compare-algos.py
@@ -1,20 +1,24 @@
 import numpy as numpy
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
 
 iris = load_iris()
-
 X = iris.data
 y = iris.target
 
-from sklearn.cross_validation import train_test_split
+# split data in test and train
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5)
 
-from sklearn import tree
-classifier = tree.DecisionTreeClassifier()
+treeClassifier = tree.DecisionTreeClassifier()
+treeClassifier.fit(X_train, y_train)
+treePreditions = treeClassifier.predict(X_test)
 
-classifier.fit(X_train, y_train)
+KNClassifier = KNeighborsClassifier()
+KNClassifier.fit(X_train, y_train)
+KNPredictions = KNClassifier.predict(X_test)
 
-preditions = classifier.predict(X_test)
-
-from sklearn.metrics import accuracy_score
-print accuracy_score(y_test, preditions)
+print 'Decision Tree Accuracy: ', accuracy_score(y_test, treePreditions)
+print 'K Neighbors Accuracy: ', accuracy_score(y_test, KNPredictions)
